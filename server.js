cab4440cb8422090b3c843deec53653916dbe0ab
nkean <nathankean2@gmail.com>
2018-04-17 11:52:24 -0500

    add get requests

--- server.js
+++ server.js
@@ -3,5 +3,17 @@ const express = require('express');
 const app = express();
 const PORT = 5000; // never changes, so we make it all caps (any port from 1025 - 65535)
 
-app.listen(PORT);  // tells the app to listen on set port
+// req is request, res is response
+
+app.get('/', function (req, res) {
+    res.send('Hello World');
+});
+
+app.get('/hey', function(req, res) {
+    res.send('Howdy');
+});
+
+app.listen(PORT, function () {
+    console.log(`listening on port: ${PORT}`); // using es6 templating again, instead of concatenation
+});  // tells the app to listen on set port
 
